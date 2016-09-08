7f6c778a16814ad209bfa17c65fc7361fae19a39
Tyjow <platon.jonathan@gmail.com>
2016-09-08 17:25:19 +0200

    retravail du site

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('.welcome');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-inverse").addClass('scroll');
+          $(".navbar").addClass('scroll');
+          $(".navbar-brand").addClass('scroll');
+       } else {
+          $(".navbar-inverse").removeClass('scroll');
+          $(".navbar").removeClass('scroll');
+          $(".navbar-brand").removeClass('scroll');
+       }
+   });
+    }
+});
